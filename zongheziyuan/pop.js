b6ea36b47b7f7864edf9dae02e987c4dd5926a37
muyun123 <53244652+muyun123@users.noreply.github.com>
2020-08-10 10:37:31 +0800

    0810.0

--- zongheziyuan/pop.js
+++ zongheziyuan/pop.js
@@ -2,18 +2,24 @@ const puppeteer = require('puppeteer');
 var option = {
     headless: false
 }
-pupInit()
-async function pupInit() {
-    var browser = await puppeteer.launch(option);
-    getTit(browser);
-    // await browser.close();
+var ele = {
+    // url:'http://www.aishoujizy.com',
+    // el:"#ajaxResult_fy .lbbt_c00",
+    url:'https://www.bingfengyl.com/',
+    el:"#iautobox .newadd li",
 }
-
-async function getTit(browser) {
+pupInit(ele)
+async function pupInit(ele) {
+    var browser = await puppeteer.launch(option);
+    var arr = await getTit(browser,ele);
+    await browser.close();
+    console.log(arr)
+}
+// 获取最新文章
+async function getTit(browser,el) {
     var page = await browser.newPage();
-    var time = ttime();
-    await page.goto('http://www.aishoujizy.com');
-    var arr = await page.$$eval("#ajaxResult_fy .lbbt_c00", (divs) => {
+    await page.goto(el.url);
+    var arr = await page.$$eval(el.el, (divs) => {
         var arr = [];
         for (var item of divs) {
             var obj = {};
@@ -24,7 +30,7 @@ async function getTit(browser) {
         }
         return arr;
     });
-    arrFilter(arr)
+   return arrFilter(arr)
 }
 // 过滤非今天的数据
 function arrFilter(arr){
@@ -34,14 +40,14 @@ function arrFilter(arr){
             newarr.push(item);
         }
     }
-    console.log(newarr.length);
+    return newarr;
 }
+// 获取今日日期 08-08
 function ttime() {
     var time = new Date();
     var month = time.getMonth() + 1;
     var day = time.getDate();
     return odd(month) + '-' + odd(day);
-    // console.log(odd(month) + '-' + odd(day))
 }
 function odd(a) {
     if (a < 10) {
@@ -60,7 +66,7 @@ Date.prototype.format = function (format) {
         "S": this.getMilliseconds() //millisecond
     }
     if (/(y+)/.test(format))
-        format = format.replace( .$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
     for (var k in o)
         if (new RegExp("(" + k + ")").test(format))
             format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
